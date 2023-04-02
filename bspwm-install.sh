cdcb54a9be69749ec47348e71e6cb182cba3804b
Tommi Niemi <tommi@niemi.lol>
2023-04-02 13:20:53 +0300

    remove unneseccary hiddening from configs

--- bspwm-install.sh
+++ bspwm-install.sh
@@ -3,8 +3,8 @@
 # Easy setup for testing
 mkdir ~/.local/share/fonts
 cp -R IosevkaTermNerdFontComplete.ttf ~/.local/share/fonts
-cp -R .config/* ~/.config/
+cp -R config/* ~/.config/
 chmod -R +x ~/.config/bspwm
 chmod -R +x ~/.config/polybar/scripts
-sudo pacman -Syu --needed --noconfirm - < packages-repository.txt
+sudo pacman -Syu --needed --noconfirm - <packages-repository.txt
 sudo fc-cache -f -v
