7a0a5c9861cdd06555695bc4b567346768fb8f59
Tommi Niemi <tommi@niemi.lol>
2023-04-03 10:32:57 +0300

    add chaos theme on zsh

---
+++ config/zsh/aliases.zsh
@@ -0,0 +1,17 @@
+# Default apps
+alias vi=nvim
+alias ls=lsd
+alias cp=xcp
+alias x=lf
+alias h=htop
+alias dd=bcp
+
+# pnpm
+export PNPM_HOME="/home/$USER/.local/share/pnpm"
+export PATH="$PNPM_HOME:$PATH"
+# pnpm end
+
+# SSH_AUTH_SOCK
+export SSH_AUTH_SOCK="$XDG_RUNTIME_DIR/ssh-agent.socket"
+
+# 
